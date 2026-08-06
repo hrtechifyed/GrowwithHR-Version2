3da8e4715f7e356b71b98cbe348d2bd30f0687d0
HRTechify <hrtechifyed@gmail.com>
2026-08-06 21:01:43 +0530

    Activate ESI Wave 4B router

--- server-entry.js
+++ server-entry.js
@@ -6,7 +6,7 @@ const {
 } = require("./server-m7-operational-readiness");
 const {
     handleSharedLegalExplanationRequest
-} = require("./server-legal-explanation-router-wave4a");
+} = require("./server-legal-explanation-router-wave4b");
 const { handleLegalExplanationRequest } = require("./server-legal-explanation");
 const { handleOperationalExplanationRequest } = require("./server-operational-explanation");
 const { handleM4DeliveryRequest } = require("./server-m4-delivery");
